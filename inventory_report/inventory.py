e26761f395ced2bb0e639a5b9ea0b3eff9a0fe91
jeferson-viana <jefersonv28@gmail.com>
2023-10-31 17:54:07 -0300

    Requsito 5 concluído

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,23 @@
+from typing import Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self,
+                 data: Optional[list[Product]] = None) -> None:
+        if data is not None:
+            self.data = data
+
+    @property
+    def data(self) -> list[Product]:
+        return self.data
+
+    @data.setter
+    def data(self) -> None:
+        self.data: list[Product] = []
+
+    def add_data(self, data: list[Product]) -> None:
+        # essa linha substitui o for
+        # self.data.extend(data)
+        for product in data:
+            self.data.append(product)
