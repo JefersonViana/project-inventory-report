0910eb6420b738e1c52f228dfe4c996cf01dce00
jeferson-viana <jefersonv28@gmail.com>
2023-10-31 16:20:42 -0300

    Requisito 3 concluído

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,15 +1,24 @@
 from typing import Dict, Type
+from inventory_report.product import Product
+import abc
 
 
-class Importer:
+class Importer(abc.ABC):
+    def __init__(self, path: str) -> None:
+        super().__init__()
+        self.path = path
+        self._import_data: list[Product] = []
+
+    @abc.abstractmethod
+    def import_data(self) -> list[Product]:
+        return self._import_data
+
+
+class JsonImporter(Importer):
     pass
 
 
-class JsonImporter:
-    pass
-
-
-class CsvImporter:
+class CsvImporter(Importer):
     pass
 
 
