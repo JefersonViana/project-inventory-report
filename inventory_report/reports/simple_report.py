5e3b253d3f1490a342cafc1c18b7a0a6767deec2
jeferson-viana <jefersonv28@gmail.com>
2023-10-31 23:47:43 -0300

    Requsito 7 concluído

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,52 @@
+from inventory_report.inventory import Inventory
+from datetime import datetime
+from collections import Counter
+import abc
+from typing import Any
+
+
 class SimpleReport:
-    pass
+    def __init__(self) -> None:
+        self.inventories: list[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    @abc.abstractmethod
+    def find_empresa(self, inventory: Inventory, fixed: int):
+        empresa: Any = ""
+        fix = fixed
+        if len(inventory.data) > fix:
+            current_empresa = Counter(
+                [product.company_name for product in inventory.data]
+            )
+            empresa = max(current_empresa, key=current_empresa.get)
+            fix = current_empresa[empresa]
+        return (empresa, fix)
+
+    def generate(self) -> str:
+        empresa = ""
+        fixed = 0
+        expiration = datetime.strptime("9999-12-31", "%Y-%m-%d")
+        manufacturing = datetime.strptime("9999-12-31", "%Y-%m-%d")
+        for inventory in self.inventories:
+            for product in inventory.data:
+                current_date = datetime.strptime(product.manufacturing_date,
+                                                 "%Y-%m-%d")
+                current_date_ex = datetime.strptime(product.expiration_date,
+                                                    "%Y-%m-%d")
+                if current_date < manufacturing:
+                    manufacturing = current_date
+
+                if current_date_ex > datetime.now() and (
+                    current_date_ex < expiration
+                ):
+                    expiration = current_date_ex
+
+            empresa, fixed = self.find_empresa(inventory, fixed)
+
+        return (
+            f"Oldest manufacturing date: {manufacturing.date()} "
+            f"Closest expiration date: {expiration.date()} "
+            f"Company with the largest inventory: {empresa} "
+        )
