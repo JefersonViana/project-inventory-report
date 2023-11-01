8dc117e63731233af1794603e83ff33032ccd0be
jeferson-viana <jefersonv28@gmail.com>
2023-11-01 16:30:01 -0300

    Requsito 10 concluído com refaroração na lógica do simple_report.py

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -15,12 +15,30 @@ class SimpleReport:
         empresa: Any = ""
         fix = fixed
         if len(inventory.data) > fix:
-            current_empresa = Counter(
+            curr_empresa = Counter(
                 [product.company_name for product in inventory.data]
             )
-            empresa = max(current_empresa, key=current_empresa.get)
-            fix = current_empresa[empresa]
+            empresa = max(curr_empresa, key=curr_empresa.get)
+            fix = curr_empresa[empresa] if curr_empresa[empresa] > fix else fix
         return (empresa, fix)
+
+    def create_report(self, inventory: Inventory, exp_date: datetime,
+                      fut_date: datetime):
+        expiration = exp_date
+        manufacturing = fut_date
+        for product in inventory.data:
+            current_date = datetime.strptime(product.manufacturing_date,
+                                             "%Y-%m-%d")
+            current_date_ex = datetime.strptime(product.expiration_date,
+                                                "%Y-%m-%d")
+            if current_date < manufacturing:
+                manufacturing = current_date
+
+            if current_date_ex > datetime.now() and (
+                current_date_ex < expiration
+            ):
+                expiration = current_date_ex
+        return (manufacturing, expiration)
 
     def generate(self) -> str:
         empresa = ""
@@ -28,22 +46,14 @@ class SimpleReport:
         expiration = datetime.strptime("9999-12-31", "%Y-%m-%d")
         manufacturing = datetime.strptime("9999-12-31", "%Y-%m-%d")
         for inventory in self.inventories:
-            for product in inventory.data:
-                current_date = datetime.strptime(product.manufacturing_date,
-                                                 "%Y-%m-%d")
-                current_date_ex = datetime.strptime(product.expiration_date,
-                                                    "%Y-%m-%d")
-                if current_date < manufacturing:
-                    manufacturing = current_date
-
-                if current_date_ex > datetime.now() and (
-                    current_date_ex < expiration
-                ):
-                    expiration = current_date_ex
+            manufacturing, expiration = self.create_report(inventory,
+                                                           expiration,
+                                                           manufacturing)
 
             new_empresa, count = self.find_empresa(inventory, fixed)
             if fixed < count:
                 empresa = new_empresa
+                fixed = count
 
         return (
             f"Oldest manufacturing date: {manufacturing.date()}\n"
